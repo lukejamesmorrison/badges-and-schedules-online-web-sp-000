094eec4e8126875583ed71e4d77197ef1c297c1c
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 17:21:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  assignments = []
   #Hello, Edsger! You'll be assigned to room 1!"
   attendees.each_with_index do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+    assignments.push("Hello, #{attendee}! You'll be assigned to room #{index + 1}!")
   end
+
+  return assignments
 end

a596cd6616191b6cb677cad7ef03e77e64b2fc7e
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-23 17:20:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   #Hello, Edsger! You'll be assigned to room 1!"
-  attendees.collect do |attendee|
-    "Hello, #{attendee}! You'll be assigned to room 1!"
+  attendees.each_with_index do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end
 end
